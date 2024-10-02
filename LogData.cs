4f8d7ca24db1f9866195f6a449ec48b6f3e45b65
Venkata Samboju <vsamboju@thelcfgroup.com>
2024-10-02 10:35:09 -0400

    Add project files.

---
+++ LogData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LoggingDemo
+{
+    public class LogData
+    {
+        public int Id { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string Message{get; set; }
+        public int Count1 {  get; set; }
+        public int Count2 { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using Serilog;
+using Serilog.Sinks.MSSqlServer;
+using LoggingDemo;
+public class Program
+{
+    public static int count_Method1 = 0;
+    public static int count_Method2 = 0;
+    public static void Main(string[] args)
+    {
+        Program p = new Program();
+        LogData logData = new LogData();
+        Log.Logger = new LoggerConfiguration().WriteTo.File("Logs/ApplicationTracker.txt")
+            .CreateLogger();
+        using var factory = LoggerFactory.Create(builder =>
+        {
+            builder.AddSerilog();
+        });
+        ILogger<Program> logger = factory.CreateLogger<Program>();
+        logger.LogInformation("Hello World! Logging is {Description}", "fun");
+        Console.WriteLine("Hello Teja");
+        string input = "";
+        var stopWatch = Stopwatch.StartNew();
+        while (input!="0") 
+        {
+            Console.WriteLine("Enter your input (Either 1 - Method1 or 2 - Method2 or 0 - ExitApplication: )");
+             input = Console.ReadLine();
+            switch(input)
+            {
+                case "1":
+                    p.TestMethod1();
+                    break;
+                case "2":
+                    p.TestMethod2(); 
+                    break;
+                case "0":
+                    Console.WriteLine("Exiting application");
+                    break;
+                default:
+                    Console.WriteLine("Enter valid input");
+                    break;
+
+            }
+        }
+        stopWatch.Stop();
+        logger.LogInformation("The application is active for {Elapsed_Time} before exiting", stopWatch.Elapsed.TotalSeconds);
+        logger.LogInformation("Number of times Method1 was hit is {Method1_hits}", count_Method1);
+        logger.LogInformation("Number of times Method2 was hit is {Method2_hits}", count_Method2);
+        Console.WriteLine(logger.ToString);
+        Log.CloseAndFlush();
+    }
+
+    public void TestMethod1()
+    {
+        count_Method1++;
+    }
+
+    public void TestMethod2() 
+    {
+        count_Method2++;
+    }
+
+}
